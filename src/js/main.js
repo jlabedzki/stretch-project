61536eeecd598d51292aaf0c856819ae29cb334f
Johnny <johnnylabedzki@hotmail.com>
2021-08-06 09:20:51 -0700

    Modified eslintrc.js to include ES6. Also downloaded jquery to a local file instead of referencing a web page.

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+let data = [1, 2, 3, 4, 5];
+
+const drawBarChart = function (data, options, element) {
+  //Data parameter is an array of numbers.
+  //Options parameter is an object which gives the user customizable options for the bar chart container. Eg. width, height, etc.
+  //Element parameter is the "bar-chart" ID in the HTML body.
+  return data;
+};
